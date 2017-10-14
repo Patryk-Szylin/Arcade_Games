e1f8bb44a67ef8fba5234c65a9e273f187649f03
unknown <Jordan.blackett@gmail.com>
2017-10-14 14:16:35 +0100

    Starting Indicators/Cast Style

--- Assets/Scripts/Player/Abilities/Abilities.cs
+++ Assets/Scripts/Player/Abilities/Abilities.cs
@@ -7,37 +7,45 @@ public class Abilities : MonoBehaviour {
     [SerializeField]
     public Cop_1_Abilities abilities;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject indicator_ability_1;
+
+    // Use this for initialization
+    void Start () {
         abilities = GetComponent<Cop_1_Abilities>();
+
+        // Instantiate Abilities indicators
+        // Set as child
+        indicator_ability_1 = Instantiate(abilities.indicator);//as GameObject;
+        indicator_ability_1.SetActive(false);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.Q)) {
+
+    // Update is called once per frame
+    void Update () {
+
+        //Abilities indicators pos 
+        indicator_ability_1.transform.position = transform.position;
+        //indicator_ability_1.transform.rotation = transform.rotation;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
             //indicator
             if (abilities.quickCast)
             {
                 abilities.ability_1();
-
-
             }
             else
             {
-                //indi
-                //fire
-
+                indicator_ability_1.SetActive(true);
             }
         }
-       else if (Input.GetKeyDown(KeyCode.W))
+       else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             abilities.ability_2();
         }
-       else if (Input.GetKeyDown(KeyCode.E))
+       else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             abilities.ability_3();
         }
-       else if (Input.GetKeyDown(KeyCode.R))
+       else if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             abilities.ability_4();
         }
