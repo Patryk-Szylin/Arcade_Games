8faad76a79cc544f1c3b88f339da5a3d55d7116d
Patryk Szylin <patrykszylin93@gmail.com>
2017-11-05 17:27:53 +0000

    Initial Push of the Ability System, it contains; Base Class for Abilities, RocketLauncher and Projectile script. Also Commands to spawn the ability's projectiles

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ability : ScriptableObject
+{
+    [HideInInspector] public Projectile m_launcher;
+    public Rigidbody m_bulletPrefab;
+
+    public abstract void TriggerAbility();
+    public abstract void Initilise(Rigidbody projectileObj, Transform playerGunPos);
+
+
+
+    // This should be in utility class
+    public Vector3 GetAbilityPointInWorldSpace()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 99999f))
+        {
+            return hit.point;
+        }
+
+        return Vector3.zero;
+    }
+}
