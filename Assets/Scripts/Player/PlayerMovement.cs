c754bb4ec9f7c8414565128426be5d732ecbf64a
Unknown <q5125295@live.tees.ac.uk>
2017-11-16 20:21:03 +0000

    quick fix
    put couple test public values so 100% sure it always right.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,19 +7,23 @@ public class PlayerMovement : NetworkBehaviour
 {
     public float m_moveSpeed = 10f;
     public float m_Boosted = 1;
+    public Vector3 Test;
+    public Vector3 Test2;
     private void Start()
     {
     }
 
     public void MovePlayer(Vector3 dir)
     {
+        Test = dir;
         //m_moveSpeed = m_moveSpeed* m_Boosted;
-        if (dir.x == 1 && dir.z == 1)
+        if (dir.x == 1 && dir.z == 1 || dir.x == 1 && dir.z == -1 || dir.x == -1 && dir.z == 1 || dir.x == -1 && dir.z == -1)
         {
             dir = dir.normalized;
         }
         dir = dir * m_moveSpeed;
         dir *= Time.deltaTime;
+        Test2 = dir;
         transform.Translate(dir);
 
     }
