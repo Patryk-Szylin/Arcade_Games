2b4e1bb50fb31e250ab3306f592a13078560b709
Unknown <q5125295@live.tees.ac.uk>
2017-12-04 18:02:35 +0000

    Res/graphic Change
    Move Into same scene lobby manager got implmented  fully.

---
+++ Assets/Menu/Scripts/Lobby_Load.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lobby_Load : MonoBehaviour {
+
+    public Canvas ToSwap;
+    public Canvas Current;
+    public Canvas MainMenu;
+    public bool SwappingToLobby;
+    public bool SwappingBack;
+    public Camera C1;
+    public Camera C2;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    public void TaskOnClick()
+    {
+        ToSwap.enabled = true;
+        Current.enabled = false;
+        if (SwappingToLobby)
+        {
+            MainMenu.enabled = false;
+            C2.enabled = true;
+            C1.enabled = false;
+
+        }
+        if (SwappingBack)
+        {
+            MainMenu.enabled = true;
+            C1.enabled = true;
+            C2.enabled = false;
+        }
+
+    }
+}
