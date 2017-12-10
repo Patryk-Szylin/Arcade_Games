8cc830ae3950166ceee94ace9655abba8cc892e2
Jordan <Jordan.blackett@gmail.com>
2017-12-10 15:08:53 +0000

    Damage Text, Kill Feed, Health Bar Colours, SpawnPoint Rotation

---
+++ Assets/Kill Feed/KillFeedPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillFeedPanel : MonoBehaviour {
+
+    [SerializeField]
+    Text text;
+
+    //start - co
+
+    // Use this for initialization
+    public void Setup(string player, string source) {
+        text.text = "<b><color=magenta>" + source + "</color></b> killed <b>" + player + "</b>";
+    }
+
+    //co fade
+}
